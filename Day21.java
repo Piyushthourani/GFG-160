5ebea5833c6e43eca964b299572011db92917a87
Piyushthourani <thouranipiyush@gmail.com>
2025-06-23 18:40:46 +0530

    Day 21 - Sorting Q1

---
+++ Day21.java
@@ -0,0 +1,35 @@
+
+// Problem Link : https://www.geeksforgeeks.org/batch/gfg-160-problems/track/sorting-gfg-160/problem/sort-an-array-of-0s-1s-and-2s4231
+
+import java.util.*;
+
+public class Day21 {
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 2, 0, 1, 2, 0, 1, 2};
+        sort012(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void sort012(int[] arr) {
+        int low = 0, mid = 0, high = arr.length - 1;
+        while (mid <= high) {
+            switch (arr[mid]) {
+                case 0:
+                    swap(arr, low++, mid++);
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    swap(arr, mid, high--);
+                    break;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
