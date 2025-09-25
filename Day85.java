c59f4da5649005e79fdd4cd29fb757ef3d7865ef
Piyushthourani <thouranipiyush@gmail.com>
2025-09-25 20:30:13 +0530

    Day 85

---
+++ Day85.java
@@ -0,0 +1,32 @@
+
+// Problem Link : https://www.geeksforgeeks.org/batch/gfg-160-problems/track/tree-gfg-160/problem/inorder-traversal
+
+public class Day85 {
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        System.out.println("Inorder traversal of the given tree:");
+        printInOrder(root);
+    }
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int item) {
+            data = item;
+            left = right = null;
+        }
+    }
+    static void printInOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        printInOrder(node.left);
+        System.out.print(node.data + " ");
+        printInOrder(node.right);
+    }
+}
